914cbb428b998e9222c8ace63fdd4446960c0399
SLM <sabrina.martorelli@gmail.com>
2022-12-10 19:21:42 +0000

    Adds loop for bussiness hours

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ var today = moment().format("dddd, MMMM Do");
 //Display the current day at the top of the calender
 $('#currentDay').text(today);
 
+
+
+//Looping from 9 to 17 hs am/pm format
+var businessHours=moment(09, 'HH');
+while(businessHours.hour()< 18){
+
+    console.log(businessHours.format('hha'));
+
+    businessHours.add(1,'hour');
+}

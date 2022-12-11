a5e2ff0a8ba5486b0062d398c6486256aab397c1
SLM <sabrina.martorelli@gmail.com>
2022-12-11 12:29:50 +0000

    Completes logic for timeblockcolors

--- js/script.js
+++ js/script.js
@@ -1,44 +1,56 @@
 
 //Gets current date
 var today = moment().format("dddd, MMMM Do");
+//Gets current date with 24hs format for timeblockColour comparison 
+var currentHour = moment().format('HH');
 
 //Display the current day at the top of the calender
 $('#currentDay').text(today);
 
 //Targets container to add rows
-var container= $('.container');
+var container = $('.container');
 
 //Adds Classes to become a flex box
 container.addClass('d-flex flex-column');
 
+var timeblockColour = "";
+
 //Looping from 9 to 17 hs am/pm format
-var businessHours=moment(09, 'HH');
-while(businessHours.hour()< 18){
+var businessHours = moment(09, 'HH');
+while (businessHours.hour() < 18) {
 
-    var hh=(businessHours.format('hha'));
+    var hh = (businessHours.format('hha'));
+    //Gets hours as 24hours format for color comparison
+    var HH24 = (businessHours.format('HH'));
 
-   //Creates a row with 3 divs for each business hours
+    //Creates a row with 3 divs for each business hours
 
     container.append(`<div class="row-${hh} d-flex"></div>`)
-    var row= $(`.row-${hh}`);
+    var row = $(`.row-${hh}`);
 
     row.append(`<div class="p-3 hour">${hh}</div>`);
 
-    //Compares current time with planner to show correct colors
+    //Compares current time hour with planner to show correct colors
     //Use of variable to set the colour
-    var timeblockColour='present';
-    // if(mayor){
-   //     timeblockColour='future';
-    // }
-    // else{
-    //    timeblockColour='past'
-    // }
-    
+
+    if (HH24 > currentHour) {
+        timeblockColour = 'future';
+    }
+    else {
+        if (HH24 < currentHour) {
+            timeblockColour = 'past'
+        }
+        else {
+            timeblockColour = 'present';
+        }
+    }
+
+
     row.append(`<div class="p-3 w-100 ${timeblockColour} "><textarea></textarea></div>`);
     row.append(`<div class="p-3 saveBtn "><i class="fas fa-save"></i></div>`);
 
 
-    businessHours.add(1,'hour');
+    businessHours.add(1, 'hour');
 }
 
 
